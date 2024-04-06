de6ac3518926cd28a157451018671dec21b9789b
Rizen <123223112+Richard0070@users.noreply.github.com>
2024-04-06 21:02:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, send_file, render_template
+from PIL import Image, ImageDraw, ImageFont, ImageOps
+import requests
+import os
+import io
+
+app = Flask(__name__)
+
+@app.route("/")
+def start():
+    return "API Denzel is Running"
+    
+@app.route("/check")
+def mbsa():
+    return render_template('index.html')
+    
+@app.route('/card')
+def generate_rank_card():
+    name = request.args.get('name')
+    level = int(request.args.get('level', 0))
+    avatarurl = request.args.get('avatarurl')
+    xp = int(request.args.get('xp', 0))
+    totalxp = int(request.args.get('totalxp', 0))
+    rank = int(request.args.get('rank', 0))
+
+    base_image = Image.open('card.jpg')
+    draw = ImageDraw.Draw(base_image)
+    username_font_size = 30
+    info_font_size = 25
+    username_font = ImageFont.truetype("fonts/mont_bold.ttf", username_font_size)
+    info_font = ImageFont.truetype("fonts/mont.ttf", info_font_size)
+
+    username = name
+    if len(username) > 12:
+        username = f'{username[:12]}...'
+
+    avatar_image = Image.open(requests.get(avatarurl, stream=True).raw).convert("RGBA").resize((110, 110))
+    mask = Image.new('L', avatar_image.size, 0)
+    draw_mask = ImageDraw.Draw(mask)
+    draw_mask.ellipse((0, 0, *avatar_image.size), fill=255)
+    circular_avatar = ImageOps.fit(avatar_image, mask.size, centering=(0.5, 0.5))
+    circular_avatar.putalpha(mask)
+    coordinate = (base_image.height - circular_avatar.height) // 2
+    base_image.paste(circular_avatar, (30, coordinate), circular_avatar)
+
+    draw.text((185, 43), f'{username}', font=username_font, fill=(255, 255, 255))
+    draw.text((185, 83), f'Level {level} | XP: {xp} / {totalxp}', font=info_font, fill=(255, 255, 255))
+    draw.text((185, 115), f'Rank: {rank}', font=info_font, fill=(255, 255, 255))
+
+    img_byte_array = io.BytesIO()
+    base_image.save(img_byte_array, format='PNG')
+    img_byte_array.seek(0)
+
+    return send_file(img_byte_array, mimetype='image/png')
