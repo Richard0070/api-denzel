58f343f9371982e3d6806a37c44a1a0aa800aa2b
Rizen <123223112+Richard0070@users.noreply.github.com>
2024-04-26 21:19:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ def get_answer():
     
     bard = BardCookies(cookie_dict=cookie_dict)
     answer = bard.get_answer(question)['content']
-    image_links = response.get('links', [])  # Get image links (list)
-    images = response.get('images', [])  # Get image links (list)
+    image_links = answer['links'] # Get image links (list)
+    images = answer['images']  # Get image links (list)
    
     return jsonify({"answer": answer, "image_links": image_links, "images": images})
 
