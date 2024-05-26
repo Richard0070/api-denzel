34e0b2a9529fb7f44233c331017ea1618d77cbd6
Rizen <123223112+Richard0070@users.noreply.github.com>
2024-05-26 22:17:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 import os
 import io
 from bardapi import BardCookies
-from bs4 import BeautifulSoup
+from instascrape import Reel
 
 app = Flask(__name__)
 
@@ -15,37 +15,17 @@ CORS(app)
 def start():
     return "API Denzel is Running"
 
-def get_video(url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Connection': 'keep-alive',
-        'Upgrade-Insecure-Requests': '1'
-    }
-
-    try:
-        response = requests.get(url, headers=headers)
-        soup = BeautifulSoup(response.content, 'html.parser')
-
-        video_tag = soup.find('meta', property='og:video')
-        if not video_tag:
-            video_tag = soup.find('meta', property='og:video:url')
-
-        if video_tag:
-            return video_tag.get('content')
-    except Exception as e:
-        print(f"Error fetching video URL: {e}")
-    return None
-
-@app.route('/api/download', methods=['GET'])
+@app.route('/download', methods=['GET'])
 def download_video():
     url = request.args.get('url')
     if not url:
         return jsonify({'error': 'URL parameter is missing.'}), 400
 
     try:
-        video_link = get_video(url)
+        reel = Reel(url)
+        reel.scrape()
+        video_link = reel.video_url
+
         if video_link:
             return jsonify({'downloadLink': video_link})
         else:
