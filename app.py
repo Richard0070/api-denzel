24db3e1989bfaf415cf7a6f85ba52c40fa001186
Rizen <123223112+Richard0070@users.noreply.github.com>
2024-08-06 21:10:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "API Denzel is Running"
+    return "Denzel™ is Running"
 
 @app.route('/translate', methods=['GET'])
 def get_translation():
